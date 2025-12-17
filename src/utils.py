100596562471a6e538901d7f130c3c6bd0ea03b5
Atul Kant Tiwari <atulkanttiwari5@gmail.com>
2025-12-18 04:42:41 +0530

    Model Trained

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,29 +22,26 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
+        trained_models = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            
+        for model_name, model in models.items():
+            para = params[model_name]
 
-            model.fit(X_train, y_train)  # Train model
+            gs = GridSearchCV(model, para, cv=3, n_jobs=-1)
+            gs.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
+            best_model = gs.best_estimator_
 
-            y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
-
+            y_test_pred = best_model.predict(X_test)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
+            trained_models[model_name] = best_model
 
-        return report
+        return report, trained_models
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-
